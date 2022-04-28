943902763cf046d13ab038724f33160da8daf728
Razvan Boicu <razvanonline@yahoo.com>
2022-04-28 15:26:37 +0300

    added the camera class, but refactorized  with header and .cpp implementation

--- plane-simulation/plane-simulation/Source.cpp
+++ plane-simulation/plane-simulation/Source.cpp
@@ -1,17 +1,18 @@
 
-#include <glm.hpp>
-#include <GL/glew.h>
-#include <glfw3.h>
-
-
 #include <iostream>
+
+#include "Camera.h"
 
 
 #pragma comment (lib, "glfw3dll.lib")
 #pragma comment (lib, "glew32.lib")
 #pragma comment (lib, "OpenGL32.lib")
 
+
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
 int main() {
-	std::cout << "acum pe x86";
+	
 	return 0;
 }
