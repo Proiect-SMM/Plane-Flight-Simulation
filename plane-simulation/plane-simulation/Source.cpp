5d29e62830b8aed02a6bffc5f48b5de90add7b19
Razvan Boicu <razvanonline@yahoo.com>
2022-04-28 14:54:29 +0300

    setup  : glm, glew

--- plane-simulation/plane-simulation/Source.cpp
+++ plane-simulation/plane-simulation/Source.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+
+#include <glm.hpp>
+#include <GL/glew.h>
 #include <glfw3.h>
 
 
+#include <iostream>
+
+
 #pragma comment (lib, "glfw3dll.lib")
-//#pragma comment (lib, "glew32.lib")
-//#pragma comment (lib, "OpenGL32.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
 
 int main() {
 	std::cout << "acum pe x86";
