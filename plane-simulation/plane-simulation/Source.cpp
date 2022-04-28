44f8fdf6e18445f5a2dd3acb42741b270c4077a9
Razvan Boicu <razvanonline@yahoo.com>
2022-04-28 14:47:30 +0300

    setup : glfw

--- plane-simulation/plane-simulation/Source.cpp
+++ plane-simulation/plane-simulation/Source.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <glfw3.h>
+
+
+#pragma comment (lib, "glfw3dll.lib")
+//#pragma comment (lib, "glew32.lib")
+//#pragma comment (lib, "OpenGL32.lib")
 
 int main() {
 	std::cout << "acum pe x86";
