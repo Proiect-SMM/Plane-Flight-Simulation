7fcee36dd301e5018c0e7c91795667a5c9e9112e
Razvan Boicu <razvanonline@yahoo.com>
2022-05-11 12:11:57 +0300

    shader class abstraction

---
+++ plane-simulation/plane-simulation/Shader.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <math.h> 
+
+#include <iostream>
+#include <fstream>
+#include <glm.hpp>
+#include <sstream>
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+	Shader(const char* vertexPath, const char* fragmentPath);
+	~Shader();
+	// activate the shader
+	// ------------------------------------------------------------------------
+	void Use() const;
+	unsigned int GetID() const { return ID; }
+
+	// MVP
+	unsigned int loc_model_matrix;
+	unsigned int loc_view_matrix;
+	unsigned int loc_projection_matrix;
+
+	// utility uniform functions
+	void SetVec3(const std::string& name, const glm::vec3& value) const;
+	void SetVec3(const std::string& name, float x, float y, float z) const;
+	void SetValue(const std::string& name, float fValue) const;
+	void SetMat4(const std::string& name, const glm::mat4& mat) const;
+
+private:
+	void Init(const char* vertexPath, const char* fragmentPath);
+
+	// utility function for checking shader compilation/linking errors.
+	// ------------------------------------------------------------------------
+	void CheckCompileErrors(unsigned int shader, std::string type);
+private:
+	unsigned int ID;
+	// constructor generates the shader on the fly
+	// ------------------------------------------------------------------------
+	
+};
+
--- plane-simulation/plane-simulation/Source.cpp
+++ plane-simulation/plane-simulation/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Camera.h"
 #include <GL/glew.h>
+#include "Shader.h"
 #include <glfw3.h>
 
 #pragma comment (lib, "glfw3dll.lib")
@@ -14,11 +15,13 @@ float g_fKd = 0.5;
 float g_fKs = 0.5;
 float g_fn = 1;
 
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 Camera* pCamera = nullptr;
 
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+static unsigned int CompileShader(unsigned int type, const std::string& source);
 void scroll_callback(GLFWwindow* window, double xoffset, double yOffset);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -42,7 +45,7 @@ int main(void) {
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	//tip : shader = program ce ruleaza pe gpu
+	//tip : shader = cod ce ruleaza pe gpu
 	//glew poate fi initializat doar dupa un context valid!!!!
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Error at glew!";
@@ -60,7 +63,21 @@ int main(void) {
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); //static, nu il modificam, dynamic - il modificam si il actualizam la fiecare frame, draw - se actualizeaza la fiecare frame, fara modificare
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); 
+	//static- nu il modificam, dynamic - il modificam si il actualizam la fiecare frame, draw - se actualizeaza la fiecare frame, fara modificare
+
+	//distanta dintre vertex-uri sizeof(float) * 2 este de 2 float-uri, yup
+	//0 = de la care se incepe
+	// 2 = cate float-uri avem per vertex
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*) 0);
+
+	Shader basicShader("basic.vs", "basic.fs");
+	basicShader.Use();
+	/*unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);*/
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -70,6 +87,7 @@ int main(void) {
 
 		//--desenare triunghi
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
 		//--stop desenare triunghi
 
 		/* Swap front and back buffers */
@@ -82,6 +100,45 @@ int main(void) {
 	glfwTerminate();
 	return 0;
 }
+//
+//static unsigned int CompileShader(unsigned int type, const std::string& source) {
+//	unsigned int id = glCreateShader(type);
+//	const char* src = source.c_str();
+//	glShaderSource(id, 1, &src, nullptr);
+//	glCompileShader(id);
+//	
+//	//Error handling
+//	int result;
+//	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+//	if (result == GL_FALSE) {
+//		int length;
+//		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+//		char * message = (char*) alloca(length * sizeof(char));
+//		glGetShaderInfoLog(id, length, &length, message);
+//		std::cout << "Failed to compile shader!" << std::endl;
+//		std::cout << message << std::endl;
+//		glDeleteShader(id);
+//		return 0;
+//	}
+//
+//	return id;
+//}
+//
+//static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+//	unsigned int program = glCreateProgram();
+//	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+//	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+//
+//	glAttachShader(program, vs);
+//	glAttachShader(program, fs);
+//	glLinkProgram(program);
+//	glValidateProgram(program);
+//
+//	glDeleteShader(vs);
+//	glDeleteShader(fs);
+//
+//	return program;
+//}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	pCamera->Reshape(width, height);
