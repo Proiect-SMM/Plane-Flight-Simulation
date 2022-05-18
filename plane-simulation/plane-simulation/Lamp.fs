d11fe09eeac79ade65ac15cb885142db8f3556d9
Acatrinei Ștefan <stefan.aca74@gmail.com>
2022-05-18 21:16:37 +0300

    Updated a few of the shader files

--- plane-simulation/plane-simulation/Lamp.fs
+++ plane-simulation/plane-simulation/Lamp.fs
@@ -1,7 +1,17 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 Normal;  
+in vec3 FragPos;  
+  
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+uniform float shininess;
+
 void main()
 {
-    FragColor = vec4(1.0); // set all 4 vector values to 1.0
-}
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+} 
--- plane-simulation/plane-simulation/ShadowMapping.fs
+++ plane-simulation/plane-simulation/ShadowMapping.fs
@@ -28,24 +28,27 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
 
-    //float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
+    float bias = max(0.05 * (1.0 - dot(lightDir, normal)), 0.001);
 
-    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-    float shadow = 0.0;
-    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    for(int x = -1; x <= 1; ++x)
-    {
-        for(int y = -1; y <= 1; ++y)
-        {
-            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
-            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
-        }    
-    }
-    shadow /= 9.0;
+    //float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    
+    float shadow = 0.0f;
 
+    vec2  texelSize = 1.0/textureSize(shadowMap, 0);
+    
+   for (int x = -1; x <= 1; x++)
+   {
+          for(int y = -1; y <= 1; y++)
+          {
+                 float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x,y) * texelSize).r;
+                 shadow += currentDepth - bias > pcfDepth? 1.0: 0.0;
+          }
+   }
+   
+   shadow/=9.0f;
     if(projCoords.z > 1.0)
-            shadow = 0.0;
-        
+        shadow = 0.0;
+
     return shadow;
 }
 
@@ -53,9 +56,9 @@ void main()
 {           
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = vec3(0.3);
+    vec3 lightColor = vec3(0.8);
     // ambient
-    vec3 ambient = 0.3 * color;
+    vec3 ambient = 0.6 * color;
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
@@ -69,8 +72,11 @@ void main()
     vec3 specular = spec * lightColor;    
     
     // calculate shadow
+
+    
+
     float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
-    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
+    vec3 lighting = (ambient + (1.0 - shadow ) * (diffuse + specular)) * color;    
     
     FragColor = vec4(lighting, 1.0);
 }
